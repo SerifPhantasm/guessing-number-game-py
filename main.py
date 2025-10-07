6e82fe71ce6301049f710dce3654dcb2c6c17981
serif <serif@serif.serif>
2025-10-07 13:59:43 +0100

    hi

--- main.py
+++ main.py
@@ -4,15 +4,14 @@
 import os
 from tkinter import *
 from tkinter import ttk
+from tkinter.ttk import Label
 
 root = Tk()
 root.maxsize(1080, 1080)
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Daniel is approaching . . . Better Run!").grid(column=0, row=0)
+text = ttk.Label(root, text="Daniel is approaching . . . Better Run!")
+text.pack(pady=20)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 root.mainloop()
-
-
-
 
