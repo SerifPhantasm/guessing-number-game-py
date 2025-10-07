cb73c4c5c8914636dc292de530a9b29b58b853ba
raincandy4u <uszynskialex@gmail.com>
2025-10-07 13:58:32 +0100

    loop added for text window

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import os
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-root.maxsize(1080, 1080)
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Daniel is approaching . . . Better Run!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+while True:
+    root = Tk()
+    root.maxsize(1080, 1080)
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="Daniel is approaching . . . Better Run!").grid(column=0, row=0)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    root.mainloop()
 
 
 
