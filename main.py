24634f2b1736d84c73779c045a68f41bbb99a97f
serif <serif@serif.serif>
2025-10-14 15:28:24 +0100

    added more error handling

--- main.py
+++ main.py
@@ -10,9 +10,39 @@ IncorrectList = ["Incorrect!", "That is Incorrect!", "Try Again!"]
 
 print("You need to give 2 numbers. You will guess a number between those two numbers.")
 time.sleep(0.5)
-AskUserforn1 = int(input("What's the first number?"))
-time.sleep(0.4)
-AskUserforn2 = int(input("What's the second number?"))
+print("The second number has to be bigger than the first number.")
+time.sleep(0.5)
+# first number
+while True:
+    try:
+        AskUserforn1 = int(input("What's the first number? "))
+        time.sleep(0.4)
+        if AskUserforn1 == (int):
+            break
+        break
+    except:
+        print("Not a Number/Valid Number")
+        continue
+    
+
+while True:
+    # second number
+    try:
+        AskUserforn2 = int(input("What's the second number? "))
+        if AskUserforn2 < AskUserforn1:
+            print("The second number is too small, try a bigger number.")
+            continue
+        elif AskUserforn2 == AskUserforn1:
+            print("It can't be the same numbers.")
+            continue
+        else:
+            break
+        
+        
+    except:
+        print("Not a Number/Valid Number")
+
+
 
 TheNumber = random.randrange(AskUserforn1, AskUserforn2)
 time.sleep(0.2)
@@ -48,6 +78,6 @@ while True:
         print("error")
 
 print("The correct answer was:", TheNumberInt)
-input(".    ")
+input("    ")
 
 # This is the REAL guessing number game, where you have to guess the number 😱
